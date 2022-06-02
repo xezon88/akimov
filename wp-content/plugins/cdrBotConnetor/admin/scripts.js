0d3c273ca3713d5591ca1fe39baadaf4360eccb7
Egor <gosha31rus.14@gmail.com>
2022-06-02 18:53:13 +0300

    научил парсить табличку, подготовил к сохранению карточки

--- wp-content/plugins/cdrBotConnetor/admin/scripts.js
+++ wp-content/plugins/cdrBotConnetor/admin/scripts.js
@@ -1,11 +1,50 @@
-class wpPluginAdmin {
-  initPlugin () {
-    console.log()
+class cdrConnectorPlugin {
+  constructor () {
+    this.body = document.querySelector('#wpbody-content')
+    console.log('HELLO FROM CONNECTOR')
+  }
+
+  async getCards () {
+    let res, codeBlock
+
+    codeBlock = this.createEl('pre')
+
+    res = await this.sendAJAX('saveCard', 'hello maaaan')
+    res = res.slice(0, -1)
+
+    this.appendNodes(codeBlock, [res])
+    this.body.append(codeBlock)
+    console.log(codeBlock);
+  }
+
+  async sendAJAX (action, data) {
+    let body, response, responseHandler
+
+    body = {action, data}
+    responseHandler = res => console.log(res)
+
+    await jQuery.post(ajaxurl, body, res => response = res)
+    return response 
+  }
+
+  createEl (tag, classes = [], innerText = '') {
+    let node = document.createElement(tag)
+  
+    if (innerText != '') {node.innerText = innerText}
+    if (classes.length > 0) {
+      classes.forEach(item => node.classList.add(item))
+    }
+  
+    return node
+  }
+
+  appendNodes(parent, childrens) {
+    childrens.forEach(node => parent.append(node))
   }
 }
 
 
 window.addEventListener('load', () => {
-  let pluginTemplate = new wpPluginTmplateAdmin()
-  pluginTemplate.initPlugin()
+  let connector = new cdrConnectorPlugin()
+  connector.getCards()
 })
--- wp-content/plugins/cdrBotConnetor/cdrBotConnector.php
+++ wp-content/plugins/cdrBotConnetor/cdrBotConnector.php
@@ -1,6 +1,6 @@
-<?
- /**
-  * Plugin Name: cdr bot connector
+<?php
+  /**
+  * Plugin Name: cdrBotConnector
   * Version: 1.0
   */
 
@@ -8,39 +8,116 @@
 
   class cdrBotConnector {
     public function __construct () {
+      // add_action('wp_enqueue_scripts', [$this, 'loadPublicScripts']);
+      // add_action('admin_enqueue_scripts', [$this, 'loadAdminScripts']);
+      // add_action('admin_menu', [$this, 'addMenu']);
+      // add_action('wp_ajax_saveCard', [$this, 'saveCard']);
+
       add_action('rest_api_init', [$this, 'initSaveCardAPI']);
+      // add_shortcode( 'mathCalc', [$this, 'loadShortCodeTemplate']);
     }
+
+    public function loadPublicScripts () {
+      wp_register_style('botConnCSS', plugins_url('/templates/styles.css', __FILE__));
+      wp_register_script('botConnJS', plugins_url('/templates/scripts.js', __FILE__));
+      
+      wp_enqueue_style('botConnCSS');
+      wp_enqueue_script('botConnJS');
+    }
+
+    public function loadAdminScripts () {
+      wp_register_script('botConnAdminJS', plugins_url('./admin/scripts.js', __FILE__));
+      wp_register_style('botConnAdminCSS', plugins_url('./admin/styles.css', __FILE__));
+
+      wp_enqueue_script('botConnAdminJS');
+      wp_enqueue_style('botConnAdminCSS');
+    }
+
+    public function addMenu () {
+      add_menu_page('botConnector', 'botConnector', 'read', 'botConnector', [$this, 'loadAdminPage']);
+    }
+
+
+    public function loadShortCodeTemplate () {
+      $shortCode = file_get_contents( plugins_url('./templates/calc.php', __FILE__) );
+      return $shortCode;
+    }
+
 
     public function initSaveCardAPI () {
       $namespace = 'cdr-connector/v1';
       $route = '/save-card/';
 
-      $params = [
+      $paramsArr = [
         'methods' => 'POST',
         'callback' => [$this, 'saveCard'],
         'args' => [],
-        'permission_callback' => function () {
-          return true;
-        },
+        'permission_callback' => function () {return true;},
       ];
 
-      register_rest_route($namespace, $route, $params);
+      register_rest_route($namespace, $route, $paramsArr);
     }
+
 
     public function saveCard ($request) {
-      $reqArr = $request -> get_params();
-
+      $tableURL = 'https://docs.google.com/spreadsheets/d';
       $id = '1Q08p_WncgTX4Ep8ik1Lga59PsD3uOt4-XYf7eGwxvhM';
       $gid = '539937704';
-      $csv = file_get_contents('https://docs.google.com/spreadsheets/d/' . $id . '/export?format=csv&gid=' . $gid);
-      // $csv = explode('\r\n', $csv);
-      // $csv = explode(',', $csv[0]);
+      $urlParams = "export?format=csv&gid=$gid";
+
+      $csv = file_get_contents("$tableURL/$id/$urlParams");
+      $csv = explode("\n", $csv);
+
+      $cardsArr = [];
+      foreach ($csv as $line) {
+        $cardsArr[] = $this -> parseCard($line);
+      }
+
+      // $post_id = wp_insert_post($cardsArr[0]);
 
       print('<pre>');
-      print_r($csv);
+      print_r($request);
+      print_r($cardsArr);
       print('</pre>');
     }
-  }
 
-  $botConnector = new cdrBotConnector;
+
+    public function loadAdminPage () {
+      $adminTemplate = file_get_contents( plugins_url('./admin/index.php', __FILE__) );
+      print($adminTemplate);
+    }
+
+
+    public function parseCard($cardString) {
+      $card = explode(',', $cardString);
+
+      $resultArr = [];
+      $resultArr['public_date']      = $card[0];
+      $resultArr['id']               = $card[1];
+      $resultArr['type']             = $card[2];
+      $resultArr['role']             = $card[3];
+      $resultArr['newType']          = $card[5];
+      $resultArr['otdelka']          = $card[6];
+      $resultArr['adress']           = $card[19];
+      $resultArr['desc']             = str_replace('"', '', $card[22]);
+      $resultArr['interrior_status'] = str_replace('"', '', $card[23]);
+      $resultArr['youtube']          = str_replace('"', '', $card[25]);
+      $resultArr['anent']            = $card[26];
+      $resultArr['kontakts']         = $card[27];
+
+      $resultArr['coords'] = [
+        'x' => str_replace('"', '', $card[20]),
+        'y' => str_replace('"', '', $card[21]),
+      ];
+
+      $resultArr['post_title'] = $resultArr['adress'] ." " .$resultArr['newType'];
+      $resultArr['post_content'] = '';
+      $resultArr['post_aouthor'] = 1;
+      $resultArr['post_status'] = 'publish';
+
+
+      return $resultArr;
+    }
+  }
 ?>
+<? $botConnector = new cdrBotConnector; ?>
